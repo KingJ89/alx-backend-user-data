858c540978b51d12d1cf6db90eb97c5c60d9f383
KingJ89 <janmutewera@gmail.com>
2025-01-30 05:33:48 +0000

    Task index.p amended successfully

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
+""" Module of Index views """
 from flask import jsonify, abort
 from api.v1.views import app_views
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -18,9 +18,24 @@ def status() -> str:
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
-      - the number of each objects
+      - the number of each object
     """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
+    return jsonify({"users": User.count()})
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """ GET /api/v1/unauthorized
+    Return:
+      - unauthorized error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden
+    Return:
+      - Forbidden error
+    """
+    abort(403)
